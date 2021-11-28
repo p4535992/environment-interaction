8c416e3e896efdb0dcabcadecef3399925565275
p4535992 <tentimarco0@gmail.com>
2021-11-28 18:29:19 +0100

    little update

---
+++ src/lib/tokenActionHUD/RollHandler.ts
@@ -0,0 +1,37 @@
+export interface RollHandler {
+  preRollHandlers: any[];
+
+  i18n(toTranslate): string;
+
+  getActor(tokenId): Actor;
+
+  getItem(actor, itemId): Item;
+
+  getToken(tokenId): Token;
+
+  throwInvalidValueErr(err): void;
+
+  handleActionEvent(event, encodedValue): Promise<void>;
+
+  doHandleActionEvent(event, encodedValue): void;
+
+  addPreRollHandler(handler): void;
+
+  registerKeyPresses(event): void;
+
+  doRenderItem(tokenId, itemId): void;
+
+  isRenderItem(): boolean;
+
+  isRightClick(event): boolean;
+
+  isAlt(event): boolean;
+
+  isCtrl(event): boolean;
+
+  isShift(event): boolean;
+
+  _isMultiGenericAction(encodedValue): boolean;
+
+  _doMultiGenericAction(encodedValue): Promise<void>;
+}
